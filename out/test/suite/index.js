b1f3eb3497dfddeabdc61f6b2fd3d16984b052ba
Diablohu <Diablohu@users.noreply.github.com>
2020-10-22 21:18:37 +0800

    rebuild

--- out/test/suite/index.js
+++ out/test/suite/index.js
@@ -1,58 +1,40 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+// import * as path from 'path';
+// import * as Mocha from 'mocha';
+// import * as glob from 'glob';
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = void 0;
-const path = __importStar(require("path"));
-const Mocha = __importStar(require("mocha"));
-const glob = __importStar(require("glob"));
+// export function run(): Promise<void> {
+// 	// Create the mocha test
+// 	const mocha = new Mocha({
+// 		ui: 'tdd',
+// 	});
+// 	mocha.useColors(true);
+// 	const testsRoot = path.resolve(__dirname, '..');
+// 	return new Promise((c, e) => {
+// 		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+// 			if (err) {
+// 				return e(err);
+// 			}
+// 			// Add files to the test suite
+// 			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+// 			try {
+// 				// Run the mocha test
+// 				mocha.run(failures => {
+// 					if (failures > 0) {
+// 						e(new Error(`${failures} tests failed.`));
+// 					} else {
+// 						c();
+// 					}
+// 				});
+// 			} catch (err) {
+// 				e(err);
+// 			}
+// 		});
+// 	});
+// }
 function run() {
-    // Create the mocha test
-    const mocha = new Mocha({
-        ui: 'tdd',
-    });
-    mocha.useColors(true);
-    const testsRoot = path.resolve(__dirname, '..');
-    return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return e(err);
-            }
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        e(new Error(`${failures} tests failed.`));
-                    }
-                    else {
-                        c();
-                    }
-                });
-            }
-            catch (err) {
-                e(err);
-            }
-        });
-    });
+    return;
 }
 exports.run = run;
 //# sourceMappingURL=index.js.map
